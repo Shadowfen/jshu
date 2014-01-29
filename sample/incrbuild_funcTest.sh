e691bf575a3c588fa6aaa234d2cb241e9612b663
Dolores Scott <scottd@texas.net>
2014-01-29 17:44:49 -0600

    provide sample test scripts

---
+++ sample/incrbuild_funcTest.sh
@@ -0,0 +1,89 @@
+#!/bin/bash 
+
+# source the unit test for scripts functions
+. ../jshutest.inc
+
+
+# helper function
+# remove host and date lines from buildnumber.txt file
+stripHostDate() {
+	source="$1"
+	dest="$2"
+	grep -v '^BuildHost:' $source | grep -v '^Date' >$dest
+}
+
+# In this one, we don't source the other script because
+# we're not going to test pieces of it. We're just going
+# to run the script and evaluate what it does.
+##############################################################
+# unit test functions
+emptyBldNumFile_Test() {
+	tmpfiles_used="./ldempty_test.txt ./ldempty_test1.txt"
+	/bin/rm -f $tmpfiles_used
+
+	./increment_build.sh build ./ldempty_test.txt
+	
+	stripHostDate ./ldempty_test.txt ./ldempty_test1.txt
+	if ! diff ./ldempty_test1.txt data/empty.txt  ; then
+		/bin/rm -f $tmpfiles_used
+		return ${jshuFAIL}
+	fi
+	
+	# cleanup files
+	/bin/rm -f $tmpfiles_used
+	return ${jshuPASS}
+}
+
+
+myprodFile_superTest() {
+	tmpfiles_used="./myprod_test.txt ./myprod_test1.txt"
+	/bin/rm -f $tmpfiles_used
+	
+	cp data/myprod.txt ./myprod_test.txt
+	./increment_build.sh super ./myprod_test.txt
+	
+	stripHostDate ./myprod_test.txt ./myprod_test1.txt
+	if ! diff ./myprod_test1.txt data/myprod.sup.txt ; then
+		/bin/rm -f $tmpfiles_used
+		return ${jshuFAIL}
+	fi
+	
+	# cleanup files
+	/bin/rm -f $tmpfiles_used
+	return ${jshuPASS}
+}
+
+myprodFile_minorTest() {
+	tmpfiles_used="./myprod_test.txt ./myprod_test1.txt"
+	/bin/rm -f $tmpfiles_used
+	
+	cp data/myprod.txt ./myprod_test.txt
+	./increment_build.sh minor ./myprod_test.txt
+	
+	stripHostDate ./myprod_test.txt ./myprod_test1.txt
+	if ! diff ./myprod_test1.txt data/myprod.min.txt ; then
+		/bin/rm -f $tmpfiles_used
+		return ${jshuFAIL}
+	fi
+	
+	# cleanup files
+	/bin/rm -f $tmpfiles_used
+	return ${jshuPASS}
+}
+
+##############################################################
+# main
+##############################################################
+# initialize testsuite
+jshuInit
+
+# run unit tests on script
+jshuRunTests
+
+# result summary
+jshuFinalize
+
+echo Done.
+echo
+let tot=failed+errors
+exit $tot
---
+++ sample/incrbuild_unitTest.sh
@@ -0,0 +1,279 @@
+#!/bin/bash 
+
+# source the unit test for scripts functions
+. ../jshutest.inc
+
+# source our script that we want to test features of
+. ./increment_build.sh
+
+##############################################################
+# unit test functions
+parseVersionTest() {
+	VERSION="1.5.7"
+	parseVersion
+	if [ "$VERSION_SUPER" != "1" ] ; then
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "5" ] ; then
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "7" ] ; then
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+parseVersionShortTest() {
+	VERSION="1.5"
+	parseVersion
+	echo $VERSION_SUPER.$VERSION_MAJOR.$VERSION_MINOR
+	if [ "$VERSION_SUPER" != "1" ] ; then
+		jshu_errmsg="\$VERSION_SUPER ($VERSION_SUPER) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "5" ] ; then
+		jshu_errmsg="\$VERSION_MAJOR ($VERSION_MAJOR) != 5"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "" ] ; then
+		jshu_errmsg="\$VERSION_MINOR ($VERSION_MINOR) != \"\""
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+incrSuperTest() {
+	VERSION="1.5.7"
+	BUILDNUM=3
+	EPOCH=7003
+	parseVersion
+	incrSuper
+	if [ ${VERSION_SUPER} -ne 2 ] ; then
+		jshu_errmsg="\$VERSION_SUPER ($VERSION_SUPER) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "0" ] ; then
+		jshu_errmsg="\$VERSION_MAJOR ($VERSION_MAJOR) != 0"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "0" ] ; then
+		jshu_errmsg="\$VERSION_MINOR ($VERSION_MINOR) != 0"
+		return ${jshuFAIL}
+	fi
+	if [ "$BUILDNUM" != "1" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$EPOCH" != "7004" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 7004"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+incrMajorTest() {
+	VERSION="1.5.7"
+	BUILDNUM=3
+	EPOCH=7003
+	parseVersion
+	incrMajor
+	if [ ${VERSION_SUPER} -ne 1 ] ; then
+		jshu_errmsg="\$VERSION_SUPER ($VERSION_SUPER) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "6" ] ; then
+		jshu_errmsg="\$VERSION_MAJOR ($VERSION_MAJOR) != 6"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "0" ] ; then
+		jshu_errmsg="\$VERSION_MINOR ($VERSION_MINOR) != 0"
+		return ${jshuFAIL}
+	fi
+	if [ "$BUILDNUM" != "1" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$EPOCH" != "7004" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 7004"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+incrMinorTest() {
+	VERSION="1.5.7"
+	BUILDNUM=3
+	EPOCH=7003
+	parseVersion
+	incrMinor
+	if [ ${VERSION_SUPER} -ne 1 ] ; then
+		jshu_errmsg="\$VERSION_SUPER ($VERSION_SUPER) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "5" ] ; then
+		jshu_errmsg="\$VERSION_MAJOR ($VERSION_MAJOR) != 5"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "8" ] ; then
+		jshu_errmsg="\$VERSION_MINOR ($VERSION_MINOR) != 8"
+		return ${jshuFAIL}
+	fi
+	if [ "$BUILDNUM" != "1" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$EPOCH" != "7004" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 7004"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+incrBuildTest() {
+	VERSION="1.5.7"
+	BUILDNUM=3
+	EPOCH=7003
+	parseVersion
+	incrBuild
+	if [ ${VERSION_SUPER} -ne 1 ] ; then
+		jshu_errmsg="\$VERSION_SUPER ($VERSION_SUPER) != 1"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MAJOR" != "5" ] ; then
+		jshu_errmsg="\$VERSION_MAJOR ($VERSION_MAJOR) != 5"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION_MINOR" != "7" ] ; then
+		jshu_errmsg="\$VERSION_MINOR ($VERSION_MINOR) != 7"
+		return ${jshuFAIL}
+	fi
+	if [ "$BUILDNUM" != "4" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 4"
+		return ${jshuFAIL}
+	fi
+	if [ "$EPOCH" != "7004" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 7004"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+getBldFileName_noParamTest() {
+	bfname=$(getBldNumFilename)
+	if [ "$bfname" != "buildnumber.txt" ] ; then
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+getBldFileName_paramTest() {
+	bfname=$(getBldNumFilename mybldnum.txt)
+	if [ "$bfname" != "mybldnum.txt" ] ; then
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+createBldNumFile_newTest() {
+	tmpfiles_used="./crbldfile_test.txt"
+	/bin/rm -f $tmpfiles_used
+	
+	createBldNumFile ./crbldfile_test.txt
+	if [ ! -e ./crbldfile_test.txt ] ; then
+		jshu_errmsg="[ ! -e ./crbldfile_test.txt ] failed"
+		return ${jshuFAIL}
+	fi
+	/bin/rm -f $tmpfiles_used
+	return ${jshuPASS}
+}
+
+loadBldNumFile_emptyTest() {
+	tmpfiles_used="./ldbldfile_test.txt"
+	/bin/rm -f $tmpfiles_used
+	
+	createBldNumFile ./ldbldfile_test.txt
+	parseVersion
+	[ ! -e ./ldbldfile_test.txt ] && return ${jshuFAIL}
+	loadBldNumFile ./ldbldfile_test.txt
+	/bin/rm -f $tmpfiles_used
+
+	if [ "${PRODNAME}" = "Unknown product" ] ; then
+		jshu_errmsg="\$PRODNAME ($PRODNAME) != Unknown product"
+		return ${jshuFAIL}
+	fi
+	if [ "$PKGNAME" != "unknown_pkg" ] ; then
+		jshu_errmsg="\$PKGNAME ($PKGNAME) != unknown_pkg"
+		return ${jshuFAIL}
+	fi
+	if [ "$VERSION" != "0.0.0" ] ; then
+		jshu_errmsg="\$VERSION ($VERSION) != 0.0.0"
+		return ${jshuFAIL}
+	fi
+	if [ "$BUILDNUM" != "0" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 0"
+		return ${jshuFAIL}
+	fi
+	if [ "$EPOCH" != "0" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 0"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+loadBldNumFileTest() {
+	tmpfiles_used="./ldbldfile1_test.txt"
+	/bin/rm -f $tmpfiles_used
+
+	PRODNAME="My sample product"
+	PKGNAME="sample_prod"
+	VERSION="1.2.3"
+	parseVersion
+	BUILDNUM="145"
+	EPOCH="7009125"
+	BUILDHOST="ananke"
+	BUILDDATE="Sun Jan 26 15:57:13 CST 2014"
+	writeBldNumFile ./ldbldfile1_test.txt
+	unset PRODNAME PKGNAME VERSION BUILDNUM EPOCH
+	
+	loadBldNumFile ldbldfile1_test.txt
+	/bin/rm -f $tmpfiles_used
+	if [ "${PRODNAME}" != "My sample product" ] ; then
+		jshu_errmsg="\$PRODNAME ($PRODNAME) != My sample product"
+		return ${jshuFAIL}
+	fi
+	if [ "${PKGNAME}" != "sample_prod" ] ; then
+		jshu_errmsg="\$PKGNAME (${PKGNAME}) != sample_prod"
+		return ${jshuFAIL}
+	fi
+	if [ "${VERSION}" != "1.2.3" ] ; then
+		jshu_errmsg="\$VERSION ($VERSION) != 1.2.3"
+		return ${jshuFAIL}
+	fi
+	if [ "${BUILDNUM}" != "145" ] ; then
+		jshu_errmsg="\$BUILDNUM ($BUILDNUM) != 145"
+		return ${jshuFAIL}
+	fi
+	if [ "${EPOCH}" != "7009125" ] ; then
+		jshu_errmsg="\$EPOCH ($EPOCH) != 7009125"
+		return ${jshuFAIL}
+	fi
+	return ${jshuPASS}
+}
+
+
+##############################################################
+# main
+##############################################################
+# initialize testsuite
+jshuInit
+
+# run unit tests on script
+jshuRunTests
+
+# result summary
+jshuFinalize
+
+echo Done.
+echo
+let tot=failed+errors
+exit $tot
