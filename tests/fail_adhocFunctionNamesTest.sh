7bd6d1abbbe6ab286218ec40a1be0067d7decdf3
Dolores Scott <dscott@securelogix.com>
2016-08-04 16:53:39 +0000

    check that function names exist

---
+++ tests/fail_adhocFunctionNamesTest.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+# source the unit test for scripts functions
+. ../jshutest.inc
+
+jshuSetup() {
+    # set this variable to a space-delimited list of function names
+    # that you want to run as tests.
+    jshuTestFunctions="AlwaysPass Imaginary AlwaysPass2"
+}
+
+jshuTeardown() {
+    return 0
+}
+
+##############################################################
+# unit test functions
+AlwaysPass() {
+	return ${jshuPASS}
+}
+
+AlwaysPass2() {
+	return ${jshuPASS}
+}
+
+NormalFuncNameTest() {
+    return ${jshuPASS}
+}
+
+
+
+##############################################################
+# main
+##############################################################
+# initialize testsuite
+jshuInit
+
+# run unit tests on script
+jshuRunTests
+
+# result summary
+jshuFinalize
+
+echo Done.
+echo
+let tot=failed+errors
+exit $tot
